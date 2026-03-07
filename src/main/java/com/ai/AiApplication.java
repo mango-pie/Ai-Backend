bc60fac80cad4b0bc7828c31fdc6126ddabc31cc
mango-pie <19936458177@163.com>
2026-03-07 09:32:50 +0800

    test1

--- src/main/java/com/ai/AiApplication.java
+++ src/main/java/com/ai/AiApplication.java
@@ -1,9 +1,12 @@
 package com.ai;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+@MapperScan("com.ai.mapper")
 public class AiApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ai/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.ai.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.ai.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
